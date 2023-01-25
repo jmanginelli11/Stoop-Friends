b6b248f639130895052d29890b9f60d1244349ee
Naomi Brender <NaomiBBrender@gmail.com>
2023-01-25 12:19:31 -0500

    added a compass

--- client/App.js
+++ client/App.js
@@ -18,10 +18,12 @@ export default function App() {
       <StatusBar style='auto' />
       <MapView
         style={{ flex: 10, margin: 30 }}
-        // provider={PROVIDER_GOOGLE}
+        provider={PROVIDER_GOOGLE}
         // provider='google'
         showsUserLocation={true}
         followsUserLocation={true}
+        showsCompass={true}
+        showsMyLocationButton={true}
         initialRegion={{
           latitude: 40.688615,
           longitude: -74.018907,
@@ -38,6 +40,9 @@ export default function App() {
         >
           <Callout>
             <Text>STOOP SALE</Text>
+            <Text>Address</Text>
+            <Text>Hours</Text>
+            <Text>What kinda stuff is there</Text>
           </Callout>
         </Marker>
       </MapView>
