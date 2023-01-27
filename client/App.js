9fe6e295d8f39f4e5f99f40c469a239d6a37e53e
Naomi Brender <NaomiBBrender@gmail.com>
2023-01-27 14:29:32 -0500

    button looks nicer

--- client/App.js
+++ client/App.js
@@ -1,5 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, StyleSheet, Text, View, Button } from 'react-native';
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+} from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import Map from './components/Map';
 import Post from './components/Post';
@@ -18,21 +25,31 @@ export default function App() {
       </Text>
       <StatusBar style='auto' />
       <Map />
-      <Button
-        style={StyleSheet.title}
-        title='POST A STOOP SALE'
-        color='purple'
-        onPress={() => Alert.alert('look at you, you pressed the button')}
-      />
+
+      <View style={styles.button}>
+        <Button
+          // style={styles.fixToText}
+          title='POST A STOOP SALE'
+          color='purple'
+          onPress={() => Alert.alert('look at you, you pressed the button')}
+        />
+      </View>
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    textAlign: 'center',
+    marginBottom: 30,
+    marginHorizontal: 30,
+    backgroundColor: '#f194ff',
+    borderRadius: 10,
+  },
+});
